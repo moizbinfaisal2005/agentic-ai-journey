f3425fd75363f78425181e219d1f8c6208508d52
moizbinfaisal2005 <moizbinfaisal@gmail.com>
2026-05-23 14:16:19 +0500

    Day 2 - Python basics

---
+++ Projects.py
@@ -0,0 +1,48 @@
+name = input("Enter Your Name: ")
+age = int(input("Enter Your Age: "))
+
+cal = 18 - age
+
+if age >= 18:
+    print(f"Welcome, {name}! You are eligible.")
+else:
+    print(f"Sorry {name}, come back in {cal} years.")
+
+
+for i in range(1, 6):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
