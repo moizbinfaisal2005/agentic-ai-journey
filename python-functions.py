42ed04afc7df9277480a84292c7038aac69836b5
moizbinfaisal2005 <moizbinfaisal@gmail.com>
2026-05-24 12:58:40 +0500

    Add Some Code

--- python-functions.py
+++ python-functions.py
@@ -5,3 +5,20 @@
 #     return
 
 # avg_calc(100,150,250)
+
+
+
+
+# cities = ["Karachi","Lahore","Islamabad","Peshawer","Quetta","Multan"]
+
+# def print_len(list):
+#     print(len(list))
+
+# def print_list(list):
+#     for i in list:
+#         print(i,end=" ") 
+
+# print_list(cities)
+
+
+
