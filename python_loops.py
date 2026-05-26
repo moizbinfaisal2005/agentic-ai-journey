ab850b2ff7f3324b87b181900ab722cbccd5b27b
moizbinfaisal2005 <moizbinfaisal@gmail.com>
2026-05-26 15:01:19 +0500

    Update in Loops File

---
+++ python_loops.py
@@ -0,0 +1,51 @@
+                                # While Loop Questions For Practice
+
+  
+#Quest 1
+
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+
+
+#Quest 2
+
+# a = int(input("Enter Your Number Here: "))
+# i = 1
+
+# while i <= 10:
+#     print (f" {a} x {i} = {a * i}")
+#     i += 1
+    
+
+
+#Quest 3
+
+
+# l = [1,4,9,16,25,36,49,65,81,100]
+
+# idx = 0
+
+# while idx <= len(l)-1:
+#     print(l[idx])
+#     idx += 1
+
+
+#Quest 4
+
+
+# l = (1,4,9,16,25,36,49,65,81,100)
+
+# i =  0
+
+# user = int(input("Search Number Here: "))
+
+# while i < len(l):
+#     if (l[i] == user):
+#         print("Found")
+       
+#     i += 1 
+     
+
