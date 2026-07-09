2ce6d87126d9d0e5e0f12de19d2fd4cb1c3244da
moizbinfaisal2005 <moizbinfaisal@gmail.com>
2026-07-09 15:19:18 +0500

    Adding Code in 2nd day

--- python-functions.py
+++ python-functions.py
@@ -11,4 +11,35 @@
 
 
 
-# print(welcome("Moiz"))
+# name = "Moiz"
+# print(welcome(name))
+
+
+# def func(small_num, large_num):
+#     equation = large_num - small_num
+#     return equation
+# math = func(16,5)
+# print(math)
+
+
+
+# def name(first_name, last_name):
+#     return f"Welcome {first_name},{last_name} to the office"
+# first = input("Enter your first name: ")
+# last = input("Enter your last name: ")
+
+# print(name(first,last))
+
+
+
+
+# name = "My name is Abdul Moiz\n"
+
+# def my_func():
+#     return name*3
+
+# print(my_func())
+
+
+
+
