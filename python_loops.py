2c4546f69f8f3cdf1046b815ea465020a9822b6c
moizbinfaisal2005 <moizbinfaisal@gmail.com>
2026-08-18 17:20:16 +0500

    Changes in 2 files

--- python_loops.py
+++ python_loops.py
@@ -3,84 +3,84 @@
   
 #Quest 1
 
-# i = 100
-# while i >= 1:
-#     print(i)
-#     i -= 1
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
 
 
 
 #Quest 2
 
-# a = int(input("Enter Your Number Here: "))
-# i = 1
+a = int(input("Enter Your Number Here: "))
+i = 1
 
-# while i <= 10:
-#     print (f" {a} x {i} = {a * i}")
-#     i += 1
+while i <= 10:
+    print (f" {a} x {i} = {a * i}")
+    i += 1
     
 
 
 #Quest 3
 
 
-# l = [1,4,9,16,25,36,49,65,81,100]
+l = [1,4,9,16,25,36,49,65,81,100]
 
-# idx = 0
+idx = 0
 
-# while idx <= len(l)-1:
-#     print(l[idx])
-#     idx += 1
+while idx <= len(l)-1:
+    print(l[idx])
+    idx += 1
 
 
 #Quest 4
 
 
-# l = (1,4,9,16,25,36,49,65,81,100)
+l = (1,4,9,16,25,36,49,65,81,100)
 
-# i =  0
+i =  0
 
-# user = int(input("Search Number Here: "))
+user = int(input("Search Number Here: "))
 
-# while i < len(l):
-#     if (l[i] == user):
-#         print("Found")
+while i < len(l):
+    if (l[i] == user):
+        print("Found")
        
-#     i += 1 
+    i += 1 
      
 
-# user = input("Do You want Ascending Or Decending Order of Numbers A Or B: ")
+user = input("Do You want Ascending Or Decending Order of Numbers A Or B: ")
 
-# if user.lower() == "a":
-#     a = int(input("Enter Start Number: "))
-#     b = int(input("Enter End Number: "))
+if user.lower() == "a":
+    a = int(input("Enter Start Number: "))
+    b = int(input("Enter End Number: "))
 
-#     if a >= b:
-#       print("Your Start Number  should larger than End Number")
-#     else:
-#      for i in range(a,b):
-#       print(i)
+    if a >= b:
+      print("Your Start Number  should larger than End Number")
+    else:
+     for i in range(a,b):
+      print(i)
 
 
-# elif user.lower() == "b":
-#     a = int(input("Enter End Number: "))
-#     b = int(input("Enter Start Number: "))
+elif user.lower() == "b":
+    a = int(input("Enter End Number: "))
+    b = int(input("Enter Start Number: "))
 
-#     if a <= b:
-#       print("Your End Number should larger than Start Number")
-#     else:
-#      for i in range(a,b,-1):
-#       print(i)
+    if a <= b:
+      print("Your End Number should larger than Start Number")
+    else:
+     for i in range(a,b,-1):
+      print(i)
       
 
-# else:
-#   "Enter Correct Option Please"
+else:
+  "Enter Correct Option Please"
 
 
-# revenue = [40,50,70]
-# expenses = [60,70,90]
+revenue = [40,50,70]
+expenses = [60,70,90]
 
-# for i in range (len(revenue)):
-#     profit = revenue[i] - expenses[i]
-#     margin = profit * 100 / revenue[i]
-#     print(f"Margin {i} :  {margin: .2f}")
+for i in range (len(revenue)):
+    profit = revenue[i] - expenses[i]
+    margin = profit * 100 / revenue[i]
+    print(f"Margin {i} :  {margin: .2f}")
